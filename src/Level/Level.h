0cf89f46b7c8f5fc2746ee6fce690673919aa6f6
nenikitov <nenikitov@mail.ru>
2021-04-14 09:42:31 -0400

    Cleaned up level data

--- src/Level/Level.h
+++ src/Level/Level.h
@@ -9,9 +9,6 @@ class Level
 	public:
 		Level(std::string levelName);
 		int tileAt(unsigned int x, unsigned int y);
-		//Level(std::string levelName, std::string tileNames[]);
-		//Tile getTileAt(unsigned int x, unsigned int y);
-		//void updateTileAt(Tile tile, unsigned int x, unsigned int y);
 	private:
 		int** levelData;
 		unsigned int* widths;
