6f9c0b3dd8cf8de273db6d9d9a8ee889b969a0b2
nenikitov <nenikitov@mail.ru>
2021-04-16 09:40:45 -0400

    Started creating tile texture loading

--- src/Tile/Tile.cpp
+++ src/Tile/Tile.cpp
@@ -1,2 +1,21 @@
 #include "Tile.h"
 
+Tile::Tile(std::string tileName)
+{
+	Json::Value json;
+	std::ifstream ifs;
+	ifs.open("data/tiles/" + tileName);
+	Json::CharReaderBuilder builder;
+	JSONCPP_STRING errs;
+	if (!parseFromStream(builder, ifs, &json, &errs))
+	{
+		throw std::invalid_argument("Recieved invalid JSON file");
+	}
+
+	
+}
+
+CHAR_INFO Tile::sample(TileTypes type, double x, double y, int lightness)
+{
+	return CHAR_INFO();
+}
--- src/Tile/Tile.h
+++ src/Tile/Tile.h
@@ -1,11 +1,16 @@
 #pragma once
 #include <string>
+#include <Windows.h>
+#include <fstream>
+#include <iostream>
+#include "TileTypes.h"
+#include "../JsonCpp/json/json.h"
 
 class Tile
 {
 	public:
 		Tile(std::string tileName);
-		CHAR_INFO sample();
+		CHAR_INFO sample(TileTypes type, double x, double y, int lightness);
 	private:
 		unsigned short** textureBrightness;
 		WORD** textureColors;
