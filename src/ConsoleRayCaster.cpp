b81513d180ea1c0f088443cb9c370c065737a17a
nenikitov <nenikitov@mail.ru>
2021-04-16 14:28:22 -0400

    Removed prints to the screen, moved them to title

--- src/ConsoleRayCaster.cpp
+++ src/ConsoleRayCaster.cpp
@@ -49,23 +49,8 @@ int main()
 
         CHAR_INFO* render = renderer.render(CONSOLE_WIDTH, CONSOLE_HEIGHT, FOV, WALL_HEIGHT);
 
-        for (unsigned int i = 0; i < 8; i++)
-            render[i].Attributes = BACKGROUND_BLUE | FOREGROUND_RED | FOREGROUND_INTENSITY;
-
-        render[7].Char.AsciiChar = '0' + fmod(DELTA_TIME * 1000000, 1) * 10;
-        render[6].Char.AsciiChar = '0' + fmod(DELTA_TIME * 100000, 1) * 10;
-        render[5].Char.AsciiChar = '0' + fmod(DELTA_TIME * 10000, 1) * 10;
-        render[4].Char.AsciiChar = '0' + fmod(DELTA_TIME * 1000, 1) * 10;
-        render[3].Char.AsciiChar = '0' + fmod(DELTA_TIME * 100, 1) * 10;
-        render[2].Char.AsciiChar = '0' + fmod(DELTA_TIME * 100, 1) * 10;
-        render[1].Char.AsciiChar = '0' + fmod(DELTA_TIME * 10, 1) * 10;
-        render[0].Char.AsciiChar = '0' + fmod(DELTA_TIME, 1) * 10;
-
-        for (unsigned int i = 0; i < 5; i++)
-            render[i + CONSOLE_WIDTH].Attributes = BACKGROUND_BLUE | FOREGROUND_RED | FOREGROUND_INTENSITY;
-
-        int frameRate = 1 / DELTA_TIME;
-        std::string title = "Console Ray Caster: FPS - " + std::to_string(frameRate);
+        const int FPS = 1 / DELTA_TIME;
+        std::string title = "Console Ray Caster: FPS - " + std::to_string(FPS) + ", Frame Time - " + std::to_string(DELTA_TIME);
         consoleHandler.setTitle(title.c_str());
 
         consoleHandler.printChars(render, 0, 0, CONSOLE_WIDTH, CONSOLE_HEIGHT);
