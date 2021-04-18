c6e2337b15b598f27ec788a193011526167ee134
nenikitov <nenikitov@mail.ru>
2021-04-18 18:13:54 -0400

    Implemented proper error handling when loading the level

--- src/Level/Level.cpp
+++ src/Level/Level.cpp
@@ -9,22 +9,35 @@ Level::Level(std::string levelName)
 	ifs.open("data/levels/" + levelName);
 	Json::CharReaderBuilder builder;
 	JSONCPP_STRING errs;
+
 	if (!parseFromStream(builder, ifs, &json, &errs))
-		throw std::invalid_argument("Recieved invalid JSON file");
+		throw std::invalid_argument(levelName + " - recieved invalid JSON file");
 
 	this->height = json["tileData"]["wall"].size();
+
+	if (this->height == 0)
+		throw std::invalid_argument(levelName + " - the height of a level is 0");
+
 	this->levelData = new int* [this->height];
 	this->widths = new unsigned int[height];
 
 	for (unsigned int y = 0; y < this->height; y++)
 	{
 		const unsigned int WIDTH = json["tileData"]["wall"][y].size();
+
+		if (WIDTH == 0)
+			throw std::invalid_argument(levelName + " - the width of a level on the line " + std::to_string(y) + " is 0");
+
 		this->widths[y] = WIDTH;
 		this->levelData[y] = new int[WIDTH];
 
 		for (unsigned int x = 0; x < WIDTH; x++)
 		{
 			const unsigned int TILE = json["tileData"]["wall"][y][x].asInt();
+		
+			if (TILE < 0)
+				throw std::invalid_argument(levelName + " - invalid tile index at " + std::to_string(x) + ", " + std::to_string(y));
+
 			this->levelData[y][x] = TILE;
 		}
 	}
