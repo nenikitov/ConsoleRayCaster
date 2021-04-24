007f5b8fb964ea6a678ab06db7428da036b41d32
nenikitov <nenikitov@mail.ru>
2021-04-24 15:08:30 -0400

    Documented console handler

--- src/ConsoleHandler/ConsoleHandler.cpp
+++ src/ConsoleHandler/ConsoleHandler.cpp
@@ -2,29 +2,35 @@
 
 bool ConsoleHandler::initialize()
 {
+	// Get the handle
 	this->newStdOut = GetStdHandle(STD_ERROR_HANDLE);
 	if (this->newStdOut == INVALID_HANDLE_VALUE)
 		return false;
 	
+	// Get the mode
 	if (!GetConsoleMode(this->newStdOut, &originalOutMode))
 		return false;
 
+	// Set the new mode
 	this->outMode = this->originalOutMode | this->REQUESTED_OUT_MODES;
 	if (!SetConsoleMode(this->newStdOut, this->outMode))
 		return false;
 
+	// Set the buffer to the console handler
 	if (!SetConsoleActiveScreenBuffer(this->newStdOut))
 		return false;
 
+	// Get the cursor
 	CONSOLE_CURSOR_INFO cursorInfo;
 	if (!GetConsoleCursorInfo(this->newStdOut, &cursorInfo))
 		return false;
 
+	// Make cursor invisible
 	cursorInfo.bVisible = false;
-
 	if (!SetConsoleCursorInfo(this->newStdOut, &cursorInfo))
 		return false;
 
+	// All is fine - return that everything is ready to go
 	return true;
 }
 
--- src/ConsoleHandler/ConsoleHandler.h
+++ src/ConsoleHandler/ConsoleHandler.h
@@ -6,10 +6,34 @@
 class ConsoleHandler
 {
 	public:
+		/// <summary>
+		/// Initialize the console handler
+		/// </summary>
+		/// <returns>If it can be initialized</returns>
 		bool initialize();
+		/// <summary>
+		/// Print the rectangle of characters
+		/// </summary>
+		/// <param name="characters">Character buffer</param>
+		/// <param name="x">Start X coordinate</param>
+		/// <param name="y">Start Y coordinate</param>
+		/// <param name="lengthX">The length of the buffer</param>
+		/// <param name="lengthY">The height of the buffer</param>
 		void printChars(CHAR_INFO* characters, SHORT x, SHORT y, SHORT lengthX, SHORT lengthY);
+		/// <summary>
+		/// Set the title of the console window
+		/// </summary>
+		/// <param name="title">Target title</param>
 		void setTitle(LPCSTR title);
+		/// <summary>
+		/// Get the width of the console
+		/// </summary>
+		/// <returns>The width of the console (in characters)</returns>
 		int getConsoleWidth();
+		/// <summary>
+		/// Get the height of the console
+		/// </summary>
+		/// <returns>The height of the console (in characters)</returns>
 		int getConsoleHeight();
 	private:
 		const DWORD REQUESTED_OUT_MODES = ENABLE_VIRTUAL_TERMINAL_PROCESSING | DISABLE_NEWLINE_AUTO_RETURN;
