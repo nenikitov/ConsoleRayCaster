2459ac290f0688398d5529a8068775b03bb724a0
nenikitov <nenikitov@mail.ru>
2021-04-24 15:11:13 -0400

    Documented controller

--- src/Controller/Controller.h
+++ src/Controller/Controller.h
@@ -4,8 +4,20 @@
 class Controller
 {
 	public:
+		/// <summary>
+		/// Get the axis value of the forward input
+		/// </summary>
+		/// <returns>The value of the forward input (-1 backward, 0 nothing, and 1 forward)</returns>
 		char getForward();
+		/// <summary>
+		/// Get the axis value of the rightward input
+		/// </summary>
+		/// <returns>The value of the rightward input (-1 lefrward, 0 nothing, and 1 rightward)</returns>
 		char getRightward();
+		/// <summary>
+		/// Get the axis value of the turn input
+		/// </summary>
+		/// <returns>The value of the turn input (-1 turn left, 0 nothing, and 1 turn rights)</returns>
 		char getTurn();
 	private:
 		unsigned const short FORWARD_KEY    = 0x57;     // W
