158f3e69e1bc6fddfb723f3d9ed4b9ba7f2fff1b
nenikitov <nenikitov@mail.ru>
2021-04-27 08:57:29 -0400

    Documented the renderer

--- src/Renderer/Renderer.cpp
+++ src/Renderer/Renderer.cpp
@@ -4,24 +4,32 @@ Renderer::Renderer(Player& player, Level& level) : player(player), level(level),
 
 CHAR_INFO* Renderer::render(unsigned short resolutionX, unsigned short resolutionY, unsigned short fov, unsigned short wallHeight)
 {
+    #pragma region Precalculate and initialize needed values
     CHAR_INFO* characters = new CHAR_INFO[resolutionX * resolutionY];
 
+    // Used for perspective correction
     const double PERPENDICULAR_LENGTH = resolutionX / 2.f / tan(fov / 2.f);
     Intersection intersection;
+    #pragma endregion
 
+    // Render column by column
     for (unsigned int x = 0; x < resolutionX; x++)
     {
+        // Angle of the ray from the X position
         const double RAY_OFFSET = (double)x - resolutionX / 2.f;
         const double ANGLE = atan(RAY_OFFSET / PERPENDICULAR_LENGTH);
         
+        // Find intersection
         intersection = this->rayCaster.findIntersection(player.getPositionX(), player.getPositionY(), player.getAngle() + ANGLE);
 
         if (intersection.intersects)
         {
+            // If the intersection happens, render the walls
             const double DELTA_X = intersection.x - player.getPositionX();
             const double DELTA_Y = intersection.y - player.getPositionY();
+            // Fix the fisheye effect
             const double PROJECTED_DISTANCE = cos(player.getAngle()) * DELTA_X + sin(player.getAngle()) * DELTA_Y;
-
+            // The heights of the wall, ceiling and floor
             const int HEIGHT = abs(wallHeight / PROJECTED_DISTANCE);
             const int CEILING = (resolutionY - HEIGHT) / 2;
             const int FLOOR = CEILING + HEIGHT;
@@ -30,16 +38,21 @@ CHAR_INFO* Renderer::render(unsigned short resolutionX, unsigned short resolutio
             {
                 if (y <= CEILING)
                 {
+                    // Ceiling rendering
                     characters[y * resolutionX + x].Attributes = ConsoleFGColors::FG_DARK_CYAN;
                     characters[y * resolutionX + x].Char.AsciiChar = '`';
                 }
                 else if (y >= FLOOR)
                 {
+                    // Floor rendering
                     characters[y * resolutionX + x].Attributes = ConsoleFGColors::FG_GREEN;
                     characters[y * resolutionX + x].Char.AsciiChar = '.';
                 }
                 else
                 {
+                    // Wall rendering
+                    // Find the point where the texture should be sampled
+                    double sampleY = (double(y) - CEILING) / HEIGHT;
                     double sampleX = 0;
                     switch (intersection.normalDirection)
                     {
@@ -56,24 +69,24 @@ CHAR_INFO* Renderer::render(unsigned short resolutionX, unsigned short resolutio
                             sampleX = -intersection.y;
                             break;
                     }
-
-                    double sampleY = (double(y) - CEILING) / HEIGHT;
+                    // Other info for texture sampling
                     double lightness = 1 - (intersection.distance / 7);
                     Tile tile = level.wallTileFrom(intersection.tile);
-
+                    // Sample the textures
                     CHAR_INFO texure = tile.sampleTexture(sampleX, sampleY, lightness, TileTypes::WALL, intersection.normalDirection);
+                    // Put it
                     characters[y * resolutionX + x] = texure;
                 }
             }
         }
         else
         {
+            // If the intersection happens, render empty characters
             for (int y = 0; y < resolutionY; y++)
             {
                 characters[y * resolutionX + x].Char.AsciiChar = ' ';
                 characters[y * resolutionX + x].Attributes = 0;
-            }
-                
+            } 
         }
     }
 
--- src/Renderer/Renderer.h
+++ src/Renderer/Renderer.h
@@ -7,7 +7,21 @@
 class Renderer
 {
 	public:
+		/// <summary>
+		/// Initialize the renderer
+		/// </summary>
+		/// <param name="player">Reference to the player from who the rendering will be performed</param>
+		/// <param name="level">Reference to the level where rendering will be performed</param>
+		/// <returns>The renderer</returns>
 		Renderer(Player& player, Level& level);
+		/// <summary>
+		/// Render the scene
+		/// </summary>
+		/// <param name="resolutionX">The number of characters on the X axis of the final render</param>
+		/// <param name="resolutionY">The number of characters on the Y axis of the final render</param>
+		/// <param name="fov">The field of view of the camera in the radians</param>
+		/// <param name="wallHeight">The wall height in character</param>
+		/// <returns>The character representation of the render</returns>
 		CHAR_INFO* render(unsigned short resolutionX, unsigned short resolutionY, unsigned short fov, unsigned short wallHeight);
 	private:
 		RayCaster rayCaster;
