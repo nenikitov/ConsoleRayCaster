845ca483a3def034027bc641e3b4ed00273e6af6
nenikitov <nenikitov@mail.ru>
2021-05-11 14:06:37 -0400

    Created sky projection

--- src/Renderer/ConsoleRenderer.cpp
+++ src/Renderer/ConsoleRenderer.cpp
@@ -68,9 +68,9 @@ CHAR_INFO* Renderer::render(unsigned short resolutionX, unsigned short resolutio
                     }
                     else
                     {
-                        Tile skyTile = level.ceilingTileFrom(1);
+                        Tile skyTile = level.ceilingTileFrom(0);
                         //TODO Create better sky rendering
-                        double sampleX = (double)x / resolutionX;
+                        double sampleX = (player.getAngle() + HOR_ANGLE) / 3.141592;
                         double sampleY = (double)y / resolutionX;
 
                         double distance = HALF_HEIGHT / tan(VERT_ANGLE) / resolutionY;
