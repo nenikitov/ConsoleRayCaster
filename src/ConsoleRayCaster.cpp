62d23d44bf3295e1c36ac8efe568afe4cf50889b
nenikitov <nenikitov@mail.ru>
2021-05-25 00:02:50 -0400

    Created a big TODO to follow later

--- src/ConsoleRayCaster.cpp
+++ src/ConsoleRayCaster.cpp
@@ -8,6 +8,10 @@
 #include "Engine/Render/BufferRenderers/Implemented/SceneRenderer.h"
 #include "Engine/Render/RenderLayerComposer/RenderLayerComposer.h"
 #include "Engine/Render/Visualizers/Implemented/Console/ASCII/ASCIIVisualizer.h"
+#include "ConsoleHandler/ConsoleHandler.h"
+#include "RayCaster/OldRayCaster.h"
+#include "Player/Player.h"
+#include "Renderer/ConsoleRenderer.h"
 
 void errorExit(std::string process, std::string exception)
 {
@@ -18,7 +22,22 @@ void errorExit(std::string process, std::string exception)
 }
 
 // TODO
-// The scene is rendering something, i dont know what. It needs further investigation
+// - File structure cleanup
+//       * Move FPS player from engine to game directory
+//       * Delete old classes
+// - Code modifications
+//       * Handle exception
+//       * Make a window that adapts to screen size
+//       * Implement more advanced lighting (fullbright texels, sector lighting)
+// - Code cleanup
+//       * Create general equations in SCENE RENDERER to increase readability
+//       * Cleanup in SCENE RENDERER
+//       * Find a better way of generating a pointer array than a bunch of switches in ASCII RENDERER
+//       * Comments and docs for new classes
+// - Further tasks
+//       * Implement simple graphics settings (setting renderer to half resolution)
+//       * Implement new cutting-edge 'SHADE' renderer that uses shading characters
+//       * Finally merge
 
 int main()
 {
@@ -26,11 +45,12 @@ int main()
     const int RENDER_HEIGHT = 63; // 63
     const double FOV = 2.0944;
 
-    Scene scene = Scene("test");
-    FPSPlayer player = FPSPlayer(scene, FOV);
+    Scene scene("test");
+
+    FPSPlayer player(scene, FOV);
     
-    SceneRenderer sceneRenderer = SceneRenderer(RENDER_WIDTH, RENDER_HEIGHT, scene, player.getCamera());
-    RenderLayerComposer composer = RenderLayerComposer(RENDER_WIDTH, RENDER_HEIGHT);
+    SceneRenderer sceneRenderer(RENDER_WIDTH, RENDER_HEIGHT, scene, player.getCamera());
+    RenderLayerComposer composer(RENDER_WIDTH, RENDER_HEIGHT);
     ASCIIVisualizer visualizer;
 
     visualizer.init();
@@ -62,59 +82,60 @@ int main()
 }
 
 /*
-
-
-ConsoleHandler consoleHandler;
-if (!consoleHandler.initialize())
+int main()
 {
-    errorExit("Console initialization", "Console does not support fast output");
-    return 1;
-}
+    ConsoleHandler consoleHandler;
+    if (!consoleHandler.initialize())
+    {
+        errorExit("Console initialization", "Console does not support fast output");
+        return 1;
+    }
 
 
-Level* level;
-try
-{
-    level = new Level("test");
-}
-catch (std::invalid_argument exception)
-{
-    errorExit("Level loading", exception.what());
-    return 1;
-}
+    Level* level;
+    try
+    {
+        level = new Level("test");
+    }
+    catch (std::invalid_argument exception)
+    {
+        errorExit("Level loading", exception.what());
+        return 1;
+    }
 
-Player player = Player(level->getPlayerStartX(), level->getPlayerStartY(), level->getPlayerStartAngle());
+    Player player = Player(level->getPlayerStartX(), level->getPlayerStartY(), level->getPlayerStartAngle());
 
 
-Renderer renderer(player, *level);
+    Renderer renderer(player, *level);
 
-auto previousTime = std::chrono::system_clock::now();
-auto currentTime = std::chrono::system_clock::now();
+    auto previousTime = std::chrono::system_clock::now();
+    auto currentTime = std::chrono::system_clock::now();
 
-unsigned const int FOV = 2.0944; // 120 degrees
+    unsigned const int FOV = 2.0944; // 120 degrees
 
-while (true)
-{
-    currentTime = std::chrono::system_clock::now();
-    const std::chrono::duration<double> DELTA_TIME_CHRONO = currentTime - previousTime;
-    const double DELTA_TIME = DELTA_TIME_CHRONO.count();
+    while (true)
+    {
+        currentTime = std::chrono::system_clock::now();
+        const std::chrono::duration<double> DELTA_TIME_CHRONO = currentTime - previousTime;
+        const double DELTA_TIME = DELTA_TIME_CHRONO.count();
 
-    previousTime = currentTime;
-    player.update(DELTA_TIME);
+        previousTime = currentTime;
+        player.update(DELTA_TIME);
 
-    unsigned const int CONSOLE_WIDTH = consoleHandler.getConsoleWidth();
-    unsigned const int CONSOLE_HEIGHT = consoleHandler.getConsoleHeight();
-    unsigned const int CONSOLE_SIZE = CONSOLE_WIDTH * CONSOLE_HEIGHT;
-    unsigned const int WALL_HEIGHT = CONSOLE_WIDTH / pow(2, FOV) / 2;
+        unsigned const int CONSOLE_WIDTH = consoleHandler.getConsoleWidth();
+        unsigned const int CONSOLE_HEIGHT = consoleHandler.getConsoleHeight();
+        unsigned const int CONSOLE_SIZE = CONSOLE_WIDTH * CONSOLE_HEIGHT;
+        unsigned const int WALL_HEIGHT = CONSOLE_WIDTH / pow(2, FOV) / 2;
 
-    CHAR_INFO* render = renderer.render(CONSOLE_WIDTH, CONSOLE_HEIGHT, FOV, WALL_HEIGHT);
+        CHAR_INFO* render = renderer.render(CONSOLE_WIDTH, CONSOLE_HEIGHT, FOV, WALL_HEIGHT);
 
-    const int FPS = 1 / DELTA_TIME;
-    std::string title = "Console Ray Caster: FPS - " + std::to_string(FPS) + ", Frame Time - " + std::to_string(DELTA_TIME);
-    consoleHandler.setTitle(title.c_str());
+        const int FPS = 1 / DELTA_TIME;
+        std::string title = "Console Ray Caster: FPS - " + std::to_string(FPS) + ", Frame Time - " + std::to_string(DELTA_TIME);
+        consoleHandler.setTitle(title.c_str());
 
-    consoleHandler.printChars(render, 0, 0, CONSOLE_WIDTH, CONSOLE_HEIGHT);
+        consoleHandler.printChars(render, 0, 0, CONSOLE_WIDTH, CONSOLE_HEIGHT);
 
-    delete[] render;
+        delete[] render;
+    }
 }
 */
