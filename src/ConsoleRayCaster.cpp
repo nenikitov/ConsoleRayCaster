0dd56f1c4c3eaf5c2f67bf9c8568869d97633377
nenikitov <nenikitov@mail.ru>
2021-05-26 10:27:02 -0400

    Deleted old files

--- src/ConsoleRayCaster.cpp
+++ src/ConsoleRayCaster.cpp
@@ -1,13 +1,12 @@
 #include <chrono>
 #include <iostream>
-#include <thread>
 #include <sstream>
 #include <string>
 #include "Engine/Scene/Scene/Scene.h"
-#include "Game/SceneObjects/FPSPlayer/FPSPlayer.h"
 #include "Engine/Render/BufferRenderers/Implemented/SceneRenderer.h"
 #include "Engine/Render/RenderLayerComposer/RenderLayerComposer.h"
 #include "Engine/Render/Visualizers/Implemented/Console/ASCII/ASCIIVisualizer.h"
+#include "Game/SceneObjects/FPSPlayer/FPSPlayer.h"
 
 void errorExit(std::string process, std::string exception)
 {
@@ -20,7 +19,7 @@ void errorExit(std::string process, std::string exception)
 // TODO
 // - File structure cleanup
 //       + Move FPS player from engine to game directory
-//       * Delete old classes
+//       + Delete old classes
 // - Code modifications
 //       + Handle exceptions
 //       * Make a window that adapts to screen size
@@ -120,62 +119,3 @@ int main()
         #pragma endregion
     }
 }
-
-/*
-int main()
-{
-    ConsoleHandler consoleHandler;
-    if (!consoleHandler.initialize())
-    {
-        errorExit("Console initialization", "Console does not support fast output");
-        return 1;
-    }
-
-
-    Level* level;
-    try
-    {
-        level = new Level("test");
-    }
-    catch (std::invalid_argument exception)
-    {
-        errorExit("Level loading", exception.what());
-        return 1;
-    }
-
-    Player player = Player(level->getPlayerStartX(), level->getPlayerStartY(), level->getPlayerStartAngle());
-
-
-    Renderer renderer(player, *level);
-
-    auto previousTime = std::chrono::system_clock::now();
-    auto currentTime = std::chrono::system_clock::now();
-
-    unsigned const int FOV = 2.0944; // 120 degrees
-
-    while (true)
-    {
-        currentTime = std::chrono::system_clock::now();
-        const std::chrono::duration<double> DELTA_TIME_CHRONO = currentTime - previousTime;
-        const double DELTA_TIME = DELTA_TIME_CHRONO.count();
-
-        previousTime = currentTime;
-        player.update(DELTA_TIME);
-
-        unsigned const int CONSOLE_WIDTH = consoleHandler.getConsoleWidth();
-        unsigned const int CONSOLE_HEIGHT = consoleHandler.getConsoleHeight();
-        unsigned const int CONSOLE_SIZE = CONSOLE_WIDTH * CONSOLE_HEIGHT;
-        unsigned const int WALL_HEIGHT = CONSOLE_WIDTH / pow(2, FOV) / 2;
-
-        CHAR_INFO* render = renderer.render(CONSOLE_WIDTH, CONSOLE_HEIGHT, FOV, WALL_HEIGHT);
-
-        const int FPS = 1 / DELTA_TIME;
-        std::string title = "Console Ray Caster: FPS - " + std::to_string(FPS) + ", Frame Time - " + std::to_string(DELTA_TIME);
-        consoleHandler.setTitle(title.c_str());
-
-        consoleHandler.printChars(render, 0, 0, CONSOLE_WIDTH, CONSOLE_HEIGHT);
-
-        delete[] render;
-    }
-}
-*/
