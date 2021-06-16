bb809d4292142c7bbc4f7f05248e7a158647c9d3
nenikitov <nenikitov@mail.ru>
2021-06-16 15:31:46 -0400

    Implemented the parser (needs testing)

--- src/Engine/Utils/CommandLineArgument/Parser/ArgumentParser.cpp
+++ src/Engine/Utils/CommandLineArgument/Parser/ArgumentParser.cpp
@@ -1,3 +1,37 @@
 #include "ArgumentParser.h"
 
+ArgumentParser::ArgumentParser()
+{
+	simpleArguments = std::vector<CommandLineArgument>();
+	simpleArgumentsFunctions = std::vector<std::function<void()>>();
+	argumentsWithOptions = std::vector<CommandLineArgument>();
+	argumentsWithOptionsMethods = std::vector<std::function<void(std::string&)>>();
+}
 
+void ArgumentParser::addSimpleArgumentToParser(const char* fullName, char shortName, std::function<void()> function)
+{
+	simpleArguments.push_back(CommandLineArgument(fullName, shortName, false));
+	simpleArgumentsFunctions.push_back(function);
+}
+
+void ArgumentParser::addArgumentWithOptionsToParser(const char* fullName, char shortName, std::function<void(std::string&)> function)
+{
+	argumentsWithOptions.push_back(CommandLineArgument(fullName, shortName, true));
+	argumentsWithOptionsMethods.push_back(function);
+}
+
+void ArgumentParser::parse(int argc, char* argv[])
+{
+	for (int i = 0; i < simpleArguments.size(); i++)
+	{
+		if (ArgumentReader::containsSimple(argc, argv, simpleArguments[i]))
+			simpleArgumentsFunctions[i]();
+	}
+
+	for (int i = 0; i < argumentsWithOptions.size(); i++)
+	{
+		std::string out = "";
+		if (ArgumentReader::containsWithFollowingArgument(argc, argv, argumentsWithOptions[i], out))
+			argumentsWithOptionsMethods[i](out);
+	}
+}
--- src/Engine/Utils/CommandLineArgument/Parser/ArgumentParser.h
+++ src/Engine/Utils/CommandLineArgument/Parser/ArgumentParser.h
@@ -1,18 +1,20 @@
 #pragma once
 
-#include "../Argument/CommandLineArgument.h"
+#include "../Reader/ArgumentReader.h"
 #include <functional>
 #include <vector>
+#include <string>
 
 class ArgumentParser
 {
 	public:
 		ArgumentParser();
-		void addSimpleArgumentToParser();
-		void addArgumentWithOptionsToParser();
+		void addSimpleArgumentToParser(const char* fullName, char shortName, std::function<void()> function);
+		void addArgumentWithOptionsToParser(const char* fullName, char shortName, std::function<void(std::string&)> function);
+		void parse(int argc, char* argv[]);
 	private:
 		std::vector<CommandLineArgument> simpleArguments;
 		std::vector<std::function<void()>> simpleArgumentsFunctions;
 		std::vector<CommandLineArgument> argumentsWithOptions;
-		std::vector<std::function<void(int&)>> argumentsWithOptionsMethods;
+		std::vector<std::function<void(std::string&)>> argumentsWithOptionsMethods;
 };
