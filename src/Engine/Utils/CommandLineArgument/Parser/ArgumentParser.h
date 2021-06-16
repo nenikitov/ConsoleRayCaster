4ac934c30094e79dfa2c67905537fd083166ff79
nenikitov <nenikitov@mail.ru>
2021-06-16 15:16:04 -0400

    Defined the methods in argument parser

--- src/Engine/Utils/CommandLineArgument/Parser/ArgumentParser.h
+++ src/Engine/Utils/CommandLineArgument/Parser/ArgumentParser.h
@@ -1,5 +1,17 @@
 #pragma once
 
+#include "../Argument/CommandLineArgument.h"
+#include <functional>
+
 class ArgumentParser
 {
+	public:
+		ArgumentParser();
+		void addSimpleArgumentToParser();
+		void addArgumentWithOptionsToParser();
+	private:
+		CommandLineArgument* simpleArguments;
+		std::function<void()>* simpleArgumentsFunctions;
+		CommandLineArgument* argumentsWithOptions;
+		std::function<void(int&)>* argumentsWithOptionsMethods;
 };
